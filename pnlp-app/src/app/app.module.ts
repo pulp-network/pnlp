c182e34d084278b60be14e8dc1e4170b05899b2e
0xd18b <70670395+0xd18b@users.noreply.github.com>
2020-07-07 11:28:51 -0400

    add layout module; remove shell module

--- pnlp-app/src/app/app.module.ts
+++ pnlp-app/src/app/app.module.ts
@@ -1,20 +1,19 @@
-import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
+import { BrowserModule } from '@angular/platform-browser';
 import { ServiceWorkerModule } from '@angular/service-worker';
-import { TranslateModule } from '@ngx-translate/core';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-
-import { environment } from '@env/environment';
-import { CoreModule } from '@core';
-import { SharedModule } from '@shared';
 import { AuthModule } from '@app/auth';
-import { HomeModule } from './home/home.module';
-import { ShellModule } from './shell/shell.module';
+import { CoreModule } from '@core';
+import { environment } from '@env/environment';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { TranslateModule } from '@ngx-translate/core';
+import { SharedModule } from '@shared';
 import { AboutModule } from './about/about.module';
-import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeModule } from './home/home.module';
+import { LayoutModule } from './layout/layout.module';
 
 @NgModule({
   imports: [
@@ -26,10 +25,10 @@ import { AppRoutingModule } from './app-routing.module';
     NgbModule,
     CoreModule,
     SharedModule,
-    ShellModule,
     HomeModule,
     AboutModule,
     AuthModule,
+    LayoutModule,
     AppRoutingModule, // must be imported as the last module as it contains the fallback route
   ],
   declarations: [AppComponent],

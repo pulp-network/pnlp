0093648e20cc5627369f9d68822c7675a6b30de3
0xd18b <70670395+0xd18b@users.noreply.github.com>
2020-07-07 10:16:02 -0400

    add LOG; bootstrap Angular app using ngx-rocket

---
+++ LOG.md
@@ -0,0 +1,9 @@
+### 2020-07-06
+
+- created repo
+- decided on core architectural elements
+- outlined basic product vision in README
+
+### 2020-07-07
+
+- cloned ngx-rocket/starter-kit to bootstrap Angular application
---
+++ pnlp-app/src/app/auth/auth-routing.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { extract } from '@app/i18n';
+import { LoginComponent } from './login.component';
+
+const routes: Routes = [{ path: 'login', component: LoginComponent, data: { title: extract('Login') } }];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+  providers: [],
+})
+export class AuthRoutingModule {}

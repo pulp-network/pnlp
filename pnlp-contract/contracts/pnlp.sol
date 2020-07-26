3188c909b4ef237c7a3efca09b7d919f80fd5716
dimetradon <>
2020-07-26 15:03:36 -0600

    Update pnlp.sol with strings instead of bytes32 and add commented out code to disallow reuse of ipfs hashes

--- pnlp-contract/contracts/pnlp.sol
+++ pnlp-contract/contracts/pnlp.sol
@@ -2,7 +2,7 @@ pragma solidity >=0.4.25 <0.7.0;
 
 contract pnlp {
     struct Publication {
-        bytes32 ipnsHash;
+        string ipnsHash;
         address publisher;
         uint256 timestamp;
     }
@@ -13,12 +13,12 @@ contract pnlp {
     }
 
     mapping (string => Publication) public publications;
-    mapping (bytes32 => Article) public articles;
+    mapping (string => Article) public articles;
 
-    event CreatedPublication(string publicationName, bytes32 ipnsHash, address publisher, uint256 timestamp);
+    event CreatedPublication(string publicationName, string ipnsHash, address publisher, uint256 timestamp);
     event PublishedArticle(string publicationName, address publisher, uint256 timestamp);
 
-    function createPublication(string memory publicationName, bytes32 ipnsHash) public {
+    function createPublication(string memory publicationName, string memory ipnsHash) public {
         // Using ipns
         // No collisions allowed!
         // record callers address
@@ -29,8 +29,13 @@ contract pnlp {
         publications[publicationName] = publication;
     }
 
-    function publishArticle(string memory publicationName, bytes32 ipfsHash) public {
+    function publishArticle(string memory publicationName, string memory ipfsHash) public {
         require(publications[publicationName].publisher == msg.sender, "articles can only be added to a publication by the publication creator");
+
+        // Unsure about this following line. If we add it in, we need to make sure that we support it.
+        // We may want to require that the hash and the publication not be reused? Need to talk to team to figure out what we want to do...
+        // require(articles[ipfsHash].publisher == address(0), "ipfsHash is already in use");
+
         emit PublishedArticle(publicationName, msg.sender, block.timestamp);
         Article memory article = Article(msg.sender, block.timestamp);
         articles[ipfsHash] = article;
